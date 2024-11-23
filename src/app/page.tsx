4e28f5f2c8567ff9dd4bb5402bf913cd13f8b99f
muhammad-stack <142173430+muhammad-stack@users.noreply.github.com>
2024-11-24 00:15:41 +0500

    Added Comments to the code to understand it in a better way

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,18 @@ import {
   UploadError,
 } from "imagekitio-next/dist/types/components/IKUpload/props";
 
+import { useState } from "react";
+
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 
 // Authenticator function to get the authentication parameters
 const authenticator = async () => {
   try {
+    // Fetching the authentication parameters from the server
     const response = await fetch("http://localhost:3000/api/auth");
 
+    // Handling the error if the response is not ok
     if (!response.ok) {
       const errorText = await response.text();
       throw new Error(
@@ -20,6 +24,7 @@ const authenticator = async () => {
       );
     }
 
+    // Parsing the response to get the data
     const data = await response.json();
     const { signature, expire, token } = data;
     return { signature, expire, token };
@@ -28,40 +33,59 @@ const authenticator = async () => {
   }
 };
 
-const onSuccess = (res: IKUploadResponse) => {
-  console.log("Success", res);
-};
-
-const onError = (err: UploadError) => {
-  console.log("Error", err);
-};
-
 // Main component
 export default function Home() {
-  const paths: string = "meme_1.jpeg";
+  // State to store the file path
+  const [filePath, setFilePath] = useState<string>("");
+
+  // On error function to handle the error
+  const onError = async (err: UploadError) => {
+    console.log("Error", err);
+  };
+
+  // On Sucssess function to handle the success
+  const onSuccess = (res: IKUploadResponse) => {
+    console.log("Success", res);
+    setFilePath(res.filePath);
+  };
+
+  // Hardcoded path for the image
+  const paths: string = `${filePath}`;
+  // HTML for displaying the UI
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center gap-4">
+      {/* ImageKitProvider is basically a wrapper as like context api  */}
       <ImageKitProvider
         urlEndpoint={urlEndpoint}
         publicKey={publicKey}
         authenticator={authenticator}
       >
-        <IKImage
-          path={paths}
-          width={300}
-          height={500}
-          transformation={[
-            {
-              raw: "l-text,i-Hello World,fs-25,l-end",
-            },
-          ]}
-          alt="Meme Loading.."
-        />
-        <IKUpload
-          fileName="test-upload.png"
-          onError={onError}
-          onSuccess={onSuccess}
-        />
+        {/* Images Display Section on Conditional Basis if the state 
+        has some value only then it is displayed otherwise not */}
+
+        {filePath && (
+          <IKImage
+            path={paths}
+            width={500}
+            height={500}
+            transformation={[
+              {
+                raw: "l-text,i-Hello World,fs-25,l-end",
+              },
+            ]}
+            alt="Meme Loading.."
+          />
+        )}
+
+        {/* Images Upload Section */}
+        <div>
+          <h2>File Upload</h2>
+          <IKUpload
+            fileName="test-upload.png"
+            onError={onError}
+            onSuccess={onSuccess}
+          />
+        </div>
       </ImageKitProvider>
     </div>
   );
