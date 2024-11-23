24a02c0ec80b4882e6535b60d1c643bb1904c557
muhammad-stack <142173430+muhammad-stack@users.noreply.github.com>
2024-11-23 21:18:17 +0500

    Till Upload functionality added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,68 @@
-import Image from "next/image";
+"use client";
+import { IKImage, IKUpload, ImageKitProvider } from "imagekitio-next";
+import {
+  IKUploadResponse,
+  UploadError,
+} from "imagekitio-next/dist/types/components/IKUpload/props";
 
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+
+// Authenticator function to get the authentication parameters
+const authenticator = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/auth");
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
+    }
+
+    const data = await response.json();
+    const { signature, expire, token } = data;
+    return { signature, expire, token };
+  } catch (error: any) {
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
+};
+
+const onSuccess = (res: IKUploadResponse) => {
+  console.log("Success", res);
+};
+
+const onError = (err: UploadError) => {
+  console.log("Error", err);
+};
+
+// Main component
 export default function Home() {
+  const paths: string = "meme_1.jpeg";
   return (
-    <div className="">
-      <img src={'https://ik.imagekit.io/tjniwo9vku/tr:w-300,h-300,l-text,pa-40,w-300,i-AMAMMA,fs-32,co-red,l-end/meme_1.jpeg'}/>
+    <div className="flex items-center justify-center">
+      <ImageKitProvider
+        urlEndpoint={urlEndpoint}
+        publicKey={publicKey}
+        authenticator={authenticator}
+      >
+        <IKImage
+          path={paths}
+          width={300}
+          height={500}
+          transformation={[
+            {
+              raw: "l-text,i-Hello World,fs-25,l-end",
+            },
+          ]}
+          alt="Meme Loading.."
+        />
+        <IKUpload
+          fileName="test-upload.png"
+          onError={onError}
+          onSuccess={onSuccess}
+        />
+      </ImageKitProvider>
     </div>
   );
 }
