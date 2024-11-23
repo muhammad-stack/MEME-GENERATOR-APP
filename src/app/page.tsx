ccda591201405226bc30e66f0a81961374377a7b
muhammad-stack <142173430+muhammad-stack@users.noreply.github.com>
2024-11-23 20:10:03 +0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <img src={'https://ik.imagekit.io/tjniwo9vku/tr:w-300,h-300,l-text,pa-40,w-300,i-AMAMMA,fs-32,co-red,l-end/meme_1.jpeg'}/>
+    </div>
+  );
+}
