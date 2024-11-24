193bd597b2e13241f883cfcb8eaa5c5c37c8827d
muhammad-stack <142173430+muhammad-stack@users.noreply.github.com>
2024-11-24 18:31:15 +0500

    Refactor SearchPage component to sanitize search input and improve error handling

--- src/app/meme/page.tsx
+++ src/app/meme/page.tsx
@@ -1,30 +1,46 @@
 import ImageKit from "imagekit";
+import { FileObject } from "imagekit/dist/libs/interfaces";
 import { unstable_noStore } from "next/cache";
 
 var imagekit: ImageKit = new ImageKit({
   publicKey: `${process.env.NEXT_PUBLIC_PUBLIC_KEY}`,
   privateKey: `${process.env.PRIVATE_KEY}`,
-  urlEndpoint: `${process.env.PRIVATE_KEY}`,
+  urlEndpoint: `${process.env.NEXT_PUBLIC_URL_ENDPOINT}`,
 });
 
-// Type definition for the search params
-type SearchParams = { q: string };
-
-const SearchPage = async (props: SearchParams) => {
+const SearchPage = async ({
+  searchParams,
+}: {
+  searchParams: { q: string };
+}) => {
   unstable_noStore();
-  const files = await imagekit.listFiles({
-    searchQuery: `name:${props.q}`,
-  });
-  return (
-    <div>
-      {files.map((file) => {
-        {
-          console.log(file);
-        }
-        return <div key={file.fileId}>{file.name}</div>;
-      })}
-    </div>
-  );
+  try {
+    // Sanitize the search input
+    const sanitizedQuery: string = encodeURIComponent(searchParams.q);
+
+    const files = await imagekit.listFiles({
+      searchQuery: `name:${sanitizedQuery}`,
+    });
+
+    console.log("Files Returned", files);
+    if (!files || files.length === 0) {
+      console.log("No file found");
+    }
+
+    return (
+      <div>
+        {files.map((file: FileObject) => {
+          // {
+          //   console.log(file);
+          // }
+          return <div key={file.fileId}>{file.name}</div>;
+        })}
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching files:", error);
+    return <div>Error loading files</div>;
+  }
 };
 
 export default SearchPage;
